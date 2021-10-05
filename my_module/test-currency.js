f8373f09641da951dc5106e91b968f020a13cf6d
Levchenko <lev75.eduard@gmail.com>
2021-10-05 22:48:50 +0300

    Старт Добавил проект my_module

---
+++ my_module/test-currency.js
@@ -0,0 +1,5 @@
+const currency = require('./currency');
+console.log('50 канадских долларов равняются этой сумме долларов США: ');
+console.log(currency.canadianToUS(50));
+console.log('30 долларов США равняются этой сумме канадских долларов: ');
+console.log(currency.USToCanadian(30));
