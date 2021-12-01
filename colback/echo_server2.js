ba94a28180c348a99dc62c4b831f867074c8111f
Levchenko <lev75.eduard@gmail.com>
2021-12-01 20:23:03 +0200

    Листинг 2.11. Простая система «публикация/подписка» с использованием генератора событий

---
+++ colback/echo_server2.js
@@ -0,0 +1,27 @@
+const events = require('events');
+const net = require('net');
+const channel = new events.EventEmitter();
+channel.clients = {};
+channel.subscriptions = {};
+
+channel.on('join', function(id, client) {
+  this.clients[id] = client;
+  this.subscriptions[id] = (senderId, message) => {
+    if (id != senderId) {
+      this.clients[id].write(message);
+    };
+  };
+  this.on('broadcast', this.subscriptions[id]);
+});
+
+const server = net.createServer(client => {
+  const id = `${client.remoteAddress}:${client.remotePort}`;
+  client.write('>_');
+  channel.emit('join', id, client);
+  client.on('data', data => {
+    data = data.toString();
+    channel.emit('broadcast', id, data);
+  });
+});
+
+server.listen(8888);
